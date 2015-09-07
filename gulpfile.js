ac490ead96ca476955d5e14c2d5afdde61f92a4b
warlyware <danward@gmail.com>
2015-09-07 15:40:11 -0400

    Add javascript to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -4,15 +4,17 @@ var copy = require('gulp-copy');
 var concat = require('gulp-concat');
 var watch = require('gulp-watch');
 var run = require('run-sequence');
+var plumber = require('gulp-plumber');
 
 var config = {
   src: {
     sass: ['./sass/*.scss'],
-    bower: ['./bower_components']
+    js: ['./js/*.js', './js/**/*.js']
   },
 
   dest: {
-    css: './public'
+    css: './public',
+    js: './public'
   }
 };
 
@@ -22,13 +24,20 @@ gulp.task('default', function(cb) {
 
 gulp.task('css', function(cb) {
   return gulp.src(config.src.sass)
+    .pipe(plumber())
     .pipe(sass({errLogToConsole: true}))
     .pipe(concat('style.css'))
     .pipe(gulp.dest(config.dest.css));
 });
 
+gulp.task('js', function(cb) {
+  return gulp.src(config.src.js)
+    .pipe(plumber())
+    .pipe(concat('script.js'))
+    .pipe(gulp.dest(config.dest.js));
+});
+
 gulp.task('watch', function() {
-  return watch(config.src.sass, function() {
-    gulp.start('css')
-  });
+  gulp.watch(config.src.sass, ['css']);
+  gulp.watch(config.src.js, ['js']);
 });
